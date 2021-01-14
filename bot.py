24633f4ca22fa861b1ee2a7ebb62f3f7394914c3
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-01-14 23:53:17 +1100

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+import random
+intents = discord.Intents.all()
+intents.members = True
+
+client = commands.Bot(command_prefix = "!", intents=intents)
+
+@client.event()
+async def on_ready(self):
+    print("MV's bot is active!")
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
