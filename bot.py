397cdaba648f9d883a872114022d91aebec8b87c
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-01-16 20:06:17 +1100

    Updated bot.py

--- bot.py
+++ bot.py
@@ -28,6 +28,26 @@ def keep_alive():
 
 keep_alive()
 
+background_task = discord.Client()
+
+async def my_background_task():
+    await background_task.wait_until_ready()
+    counter = 0
+    channel = discord.Object(id="channel_id_here")
+    while not background_task.is_closed:
+        counter += 1
+        await background_task.send_message(channel, counter)
+        await asyncio.sleep(60)
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(background_task.user.name)
+    print(background_task.user.id)
+    print("------")
+
+background_task.loop.create_task(my_background_task())
+
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
