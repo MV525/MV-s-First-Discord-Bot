f9be82af8a883e74b6d6db89c9b08f1604d8cbfa
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-01-17 20:06:14 +1100

    Updated other.py

--- cogs/other.py
+++ cogs/other.py
@@ -37,7 +37,7 @@ class other(commands.Cog):
     #Overriding help command
     @commands.command(aliases = ["HELP", "Help"])
     async def help(self, ctx):
-        await ctx.send("```\nMultiline\nhelp\ncommand```")        
+        await ctx.send("```\nCommands for moderators:\n!purge / !clear => allows you to clear any number of messages\n!kick => kicks a specified user\n!ban => bans a specified user\n!unban => unbans a specified user and automatically sends an invite link\n\nGeneral/\"fun\" commands:\n!ping => returns ping in ms\n!penisSize => tells you how big your dick is\n!dm => allows you to dm a member in the server\n!rickRoll => rick rolls every member in the guild```")        
 
 def setup(client):
     client.add_cog(other(client))
