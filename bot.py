1791159abfbb88fcb9f94b6d5bb83ac62095b928
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-01-28 14:21:38 +1100

    Updated bot.py

--- bot.py
+++ bot.py
@@ -7,27 +7,7 @@ intents.members = True
 
 client = commands.Bot(command_prefix = "!", intents=intents, help_command=None)
 
-#Flask for server
-from flask import Flask
-from threading import Thread
-
-app = Flask("")
-
-#Returns "MV's bot is active" upon successful activation
-@app.route("/")
-def main():
-  return "MV's bot is active!"
-
-#Hosting bot on port 8000
-def run():
-  app.run(host="0.0.0.0", port=8000)
-
-#Keeping bot alive
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
-
-keep_alive.keep_alive()
+keep_alive.keep_alive() #Keeping bot alive
 
 #Loads in all the required cogs
 for filename in os.listdir("./cogs"):
