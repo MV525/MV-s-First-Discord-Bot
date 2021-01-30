9fc164aa0a6c78e123c15464a23e91153696cf48
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-01-30 22:48:23 +1100

    Fixed indentation

--- keep_alive.py
+++ keep_alive.py
@@ -7,13 +7,13 @@ app = Flask("")
 #Returns "MV's bot is active" upon successful activation
 @app.route("/")
 def main():
-  return "MV's bot is active!"
+    return "MV's bot is active!"
 
 #Hosting bot on port 8000
 def run():
-  app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
 
 #Keeping bot alive
 def keep_alive():
-  server = Thread(target=run)
-  server.start()
+    server = Thread(target=run)
+    server.start()
