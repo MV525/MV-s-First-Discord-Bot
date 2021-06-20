84897749dc077be34d0e09c436222c9b245b199f
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-06-20 19:55:46 +1000

    Added asyncio

--- cogs/other.py
+++ cogs/other.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+import asyncio
 import random
 import requests
+
 
 #Creating the class and initialising to allow commands to be active
 class other(commands.Cog):
