99077493df1f145cb9ffac225f18df6fe2c3e383
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-06-20 19:25:49 +1000

    Attempt to fix reaction_response_test command

--- cogs/other.py
+++ cogs/other.py
@@ -81,7 +81,7 @@ class other(commands.Cog):
 
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.user) #1 use per user every 10 seconds
-    async def reaction_response_test(self, ctx, message):
+    async def reaction_response_test(self, ctx):
         await ctx.send("Say hello!")
 
         def check(m):
