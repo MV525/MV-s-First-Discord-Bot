ab26e0e54094e7b30902ca3a7cdd119d394e0863
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-06-20 17:59:02 +1000

    Fixed small error with defining command

--- cogs/other.py
+++ cogs/other.py
@@ -80,7 +80,7 @@ class other(commands.Cog):
             await ctx.send("You haven't specified a number or have given an incorrect value! Type !mathfact (or !mathfacts, !matHFact) followed by a number (such as: !mathfact 5)!")
 
     @commands.command()
-    async def test():
+    async def test(self, ctx):
         message = await ctx.send("TEST1")
         await asyncio.sleep(3)
         await message.edit(content="TEST")
