19d6f29537b9d7871f6e74f18c67ea5dcdb0c58b
MV525 <mohammad.vahdani@education.nsw.gov.au>
2021-06-20 20:43:53 +1000

    Re-added checker for whether or not a command is on cooldown

--- bot.py
+++ bot.py
@@ -14,6 +14,11 @@ async def on_ready():
     change_status.start()
     print(f"Logged in as {client.user.name} - {client.user.id}")
 
+@client.event
+async def on_command_error(ctx, error): #this is to check whether or not a command is on cooldown, to be used if any command ever needs to have a cooldown
+    if isinstance(error, commands.CommandOnCooldown):
+        message = f"This command is cooldown! Please try again in {str(error.retry_after)[0]}" #checks if message is on cooldown
+        await ctx.send(message) #notifies user that the command is on cooldown
 
 status_list = cycle(["being the best bot", "being the worst bot"])
 #Background task:
